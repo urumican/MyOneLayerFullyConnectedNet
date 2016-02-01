8f24c9b9f01efd07e3f4c9ae76f3f5b8d2d4e091
Xin Li <urumican@users.noreply.github.com>
2016-01-31 21:31:18 -0800

    Update MyOneLayerNet.py

--- MyOneLayerNet.py
+++ MyOneLayerNet.py
@@ -1,32 +1,13 @@
+#####################################################
+# The first homework for Deep Learning, which is to #
+# compose a one layer fully connected 2-class clas- #
+# sification net.                                   #  
+#                                                   #
+# For classification problem, we use sigmoid units  #
+# as out put layer.                                 #
+#####################################################
+
 import numpy
-
-##########  My Activation Functions ##################
-class ActivationFuctions(Object):
-	def relu(self, x):
-		if x >= 0:
-			return x
-		else: 
-			return 0.0
-	# end	
-
-	def relu_prime(self, x): 
-		if x > 0:
-			return 1.0
-		elif x = 0:
-			return 0.5
-		else 
-			return 0.0
-	# end
-
-# end
-
-##########  My Loss Functions ########################
-class LossFunctions(Object): # Functions inside should work on scaler
-	def crossEntropy(self, z, y):
-		return y * numpy.log(z) + (1.0 - y) * log(1.0 - z)
-	# end
-
-# end
 
 ########### My Neural Network ########################
 class MyOneLayerFullyConnectedNet(Object):
@@ -35,8 +16,8 @@ class MyOneLayerFullyConnectedNet(Object):
 		self.y = y # output
 		self.numOfLayers = len(param)
 		self.numOfNeuronsForAllLayers = [attr[0] for attr in param] # one-hidden 
-		self.activitionFunction = [attr[1] for attr in param]
-		self.prime = [attr[2] for attr in param]
+		self.activationFunction = [attr[1] for attr in param]
+		self.activationPrime = [attr[2] for attr in param]
 		self.build();
 	# end
 
@@ -47,7 +28,7 @@ class MyOneLayerFullyConnectedNet(Object):
 		self.outputs = []
 		self.errors = []
 		# Initiate Weights using variable 
-		for layer in range(self.numOfHiddenLayers + 1):
+		for layer in range(self.numOfHiddenLayers - 1):
 			# Weight matrices should be m-by-n 
 			n = self.numOfNeuronsForAllLayers[layer]
 			m =  self.numOfNeuronsForAllLayers[layer+1]
@@ -73,17 +54,45 @@ class MyOneLayerFullyConnectedNet(Object):
 			self.inputs[i] = slef.weights[i-1].dot(self.inputs[i-1]) + self.biases[i-1]
 			self.outputs[i] = self.activationFunction[i](self.inputs[i])
 		return self.outputs[-1] # output the final result
-	# end
+	## end #########################################################################
 
-	def weightUpDate(self, x, y): # this is where backprobagation lies.
-		output = self.feedForward(x)
-		self.errors[-1] = self.prime[-1](self.output[-1]) * (output - y) 
-	# end
+	def updateWeights(self, x, y, stepSize): # this is where backprobagation lies.
+		# Go forward
+		self.feedForward(x)
+		# Go backward
+		nabla_weights, nabla_biases = self.backPropagate(y)
+		# Start updating
+		for 
+			
+		# end
+	## end #########################################################################
 
-	def backPropagate(): 
-
-# end
-
+	######### My backpropagation is used to calculate all errors at once. ##########
+	def backPropagate(self, y):
+		#initialize matrices for derivative of W and b
+		nabla_weights = [numpy.zeros(w.shape) for w in self.weights]
+		nabla_biases = [numpy.zeros(b.shape) for b in self.biases]
+		# Calculate error for the output layer
+		self.errors[-1] = self.lossPrime(self.output[-1], y) * self.activationPrime[-1](self.inputs[-1])
+		nabla_bias[-1] = self.errors[-1]
+		nabla_weights[-1] = numpy.dot(self.errors[-1], self.outputs[-2].T)
+		
+		# Start backPropagation, calculate from the second-last layers.
+		for layer in xrange(self.numOfLayer - 2, 0, -1):
+			# Note that the w_l equals to the matrix corresponding to the index 'l-1.
+			self.errors[layer] = numpy.dot(self.weights[layer].T, self.errors[layer+1]) * self.activationPrime(self.inputs[layer])
+			# Calcualte nabla_Loss / nabla_w_l. 
+			nabla_weights[layer] = numpy.dot(self.errors[layer], self.output[layer-1].transpose())
+			# Calculate nabla_loss / nabla_b_l.
+			nabla_biases[layer] = self.errors[layer]
+		# end	
+		
+		return (nabla_weights, nabla_biases)
+	## end #########################################################################
+	
+	def predict(x)
+		
+	## end #########################################################################
 ########### My other functions  ######################
 
 
