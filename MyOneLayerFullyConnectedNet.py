f487f4751bb2e04fb12644668c5aed1443d717a1
Xin Li <urumican@users.noreply.github.com>
2016-02-03 14:37:04 -0800

    Update MyOneLayerFullyConnectedNet.py

--- MyOneLayerFullyConnectedNet.py
+++ MyOneLayerFullyConnectedNet.py
@@ -12,16 +12,18 @@
 import numpy
 
 ### My Neural Network ###
-class MyOneLayerFullyConnectedNet(Object):
+class MyOneLayerFullyConnectedNet:
 
 	## Param's format:
 	## 	Param is a tupple. The number of elements in this tuple
 	##      equals to the number of layers. Each element is a tuple
 	##	containing three kind of information, number of neurons,
 	##      activation function, and derivative of activation function.	
-	def __init__(self, train_data, train_label, param, loss, lossPrime):
+	def __init__(self, train_data, train_label, test_data, test_label, param, loss, lossPrime):
 		self.train_data = train_data # input data are all row vector
 		self.train_label = train_label # output
+		self.test_data = test_data
+		self.test_label = test_label
 		self.numOfLayers = len(param)
 		self.numOfNeuronsForAllLayers = [attr[0] for attr in param] # one-hidden 
 		self.activationFunction = [attr[1] for attr in param]
@@ -34,12 +36,14 @@ class MyOneLayerFullyConnectedNet(Object):
 	def build(self):
 		self.weights = [] # Weights matrices for all layers.
 		self.biases = [] # All biases of all layers.
-		self.imputs = []
+		self.inputs = []
 		self.outputs = []
 		self.errors = []
+		self.dw = []
+		self.db = []
 		
 		# Initiate Weights using variable 
-		for layer in range(self.numOfHiddenLayers - 1):
+		for layer in range(self.numOfLayers - 1):
 			# Weight matrices should be m-by-n 
 			m = self.numOfNeuronsForAllLayers[layer] # input dim
 			n =  self.numOfNeuronsForAllLayers[layer+1] # output dim
@@ -47,45 +51,48 @@ class MyOneLayerFullyConnectedNet(Object):
 			self.biases.append(numpy.random.normal(0, 1, (1, n)))
 			self.inputs.append(numpy.zeros((1, m)))
 			self.outputs.append(numpy.zeros((1, m)))
-			self.errors.append(numpy.zeors((1, m)))
+			self.errors.append(numpy.zeros((1, m)))
 		# end #
 
 		# last layer is output 
 		n = self.numOfNeuronsForAllLayers[-1]
-		self.imputs.append(numpy.zeros((1, n)))
+		self.inputs.append(numpy.zeros((1, n)))
 		self.outputs.append(numpy.zeros((1, n)))
 		self.errors.append(numpy.zeros((1, n)))
 	## end ##
 
-	def feedForward(self, x):
-		dim = len(x) 
-		x.shape = (dim, 1)
+	def feedForward(self, x): 
 		# Populate input
-		self.imputs[0] = x
+		self.inputs[0] = x
 		self.outputs[0] = x
+		#print 'Currnt data: ', x
 		for i in range(1, self.numOfLayers):
-			self.inputs[i] = numpy.dot(self.inputs[i-1], self.weights[i-1]) + self.bias[i-1] 
-			self.outputs[i] = [self.activationFunction[i](n) for n in self.inputs[i]]
+			self.inputs[i] = numpy.dot(self.inputs[i-1], self.weights[i-1]) + self.biases[i-1] 
+			self.outputs[i] = self.activationFunction[i](self.inputs[i]) #[self.activationFunction[i](n) for n in self.inputs[i]]
+			#print 'current output: ', self.outputs[i]
 		# end #
+		#print 'Output layer: ', self.outputs[-1]
 		return self.outputs[-1] # output the final result
 	## end ##
 
-	def updateWeights(self, dataBatch, labelBatch, batchSize, stepSize, gamma):
+	def updateWeights(self, batchSize, dataBatch, labelBatch, stepSize):
 		# Define a empty increment matrix for momentum
 		delta_w = [numpy.zeros(w.shape) for w in self.weights]
 		delta_b = [numpy.zeros(b.shape) for b in self.biases]
 		# Generate Increments
 		for i in range(batchSize):
-			data = databatch[i]
+			data = numpy.array(dataBatch[i])
+			#data.shape = (1,data.shape[0])
 			label = labelBatch[i]
 			# Go forward
 			self.feedForward(data)
 			# Go backward
 			nabla_weights, nabla_biases = self.backPropagate(label)
+			#print 'Second Layer Gradient: \n', len(nabla_weights) 
 			# Accumulate Gradients for this batch
-			for layer in xrange(self.numOfLayers - 2) 
-				delta_w_t[layer] += nabla_w[layer]
-				delta_b_t[layer] += nabla_b[layer]
+			for layer in range(self.numOfLayers - 2): 
+				delta_w[layer] += nabla_weights[layer]
+				delta_b[layer] += nabla_biases[layer]
 			# end #
 		# end #
 		
@@ -100,24 +107,59 @@ class MyOneLayerFullyConnectedNet(Object):
 		# Get the size of the data. 
 		dataSize = self.train_data.shape[0]
 		for itr in range(epoch):
+			print 'Epoch:', itr
 			randSerie = numpy.random.randint(dataSize, size = dataSize)
 			numOfBatch = dataSize / miniBatchSize
 			# Create Momentum variable
-			momentum_w = [numpy.zeros(w.shape) for w in this.weights]
-			momentum_b = [numpy.zeros(b.shape) for b in this.biases] 
+			momentum_w = [numpy.zeros(w.shape) for w in self.weights]
+			momentum_b = [numpy.zeros(b.shape) for b in self.biases] 
 			# Start batch gradient descent
-			for i in range(numOfBatch)
+			for i in range(numOfBatch):
 				# Extract my mini-batch randomly
-				miniBatchData = self.train_data[randSerie[i * miniBatchSize : i * miniBatchSize + miniBatchSize - 1]]
-				miniBatchLabel = self.train_label[randSerie[i * miniBatchSize : i * miniBatchSize + miniBatchSize - 1]]
+				miniBatchData = self.train_data[randSerie[i * miniBatchSize : i * miniBatchSize + miniBatchSize]]
+				miniBatchLabel = self.train_label[randSerie[i * miniBatchSize : i * miniBatchSize + miniBatchSize]]
 				# Get Increment
-				delta_w, delta_b = self.updateWeights(miniBatchsize, miniBatchData, miniBatchLabel, stepSize)
-				momentum_w = [gamma * mw - stepSize * dw for dw, mw in delta_w, momentum_w]
-				momentum_b = [gamma * mb - stepSize * db for db, mb in delta_b, momentum_b]	
+				delta_w, delta_b = self.updateWeights(miniBatchSize , miniBatchData, miniBatchLabel, stepSize)
+				#print 'Gradients: ', delta_w
+				for idx in range(len(delta_w)):
+					momentum_w[idx] = gamma * momentum_w[idx] - stepSize * delta_w[idx]
+					momentum_b[idx] = gamma * momentum_b[idx] - stepSize * delta_b[idx]
+				# end #	
+	
 				# Updata weights
-				self.weights = [w + mw for w, mw in self.weights, momentum_w]
-				self.biases = [b + mb for b, mb in self.biases, momentum_b]
+				for k in range(len(momentum_w)):
+					self.weights[k] = self.weights[k] + momentum_w[k]
+					self.biases[k] = self.biases[k] + momentum_b[k]
+				# end #
 			# end #
+
+			counter = 0
+			batchLoss = 0
+			for j in range(10000):
+				c, out = self.prediction(self.train_data[j])
+				#print 'output: ', out
+				batchLoss = batchLoss + self.lossFunction(out, self.train_label[j])
+				if c == self.train_label[j]:
+					counter = counter + 1
+				# end #
+			# end # 
+			batchLoss = batchLoss / 10000
+
+			# Test data part#
+			testCount = 0
+			testLoss = 0
+			for a in range(2000):
+				c, out = self.prediction(self.test_data[a])
+				testLoss = testLoss + self.lossFunction(out, self.test_label[a])
+				if c == self.test_label[a]:
+					testCount = testCount + 1
+
+			
+			print 'Weights: \n', self.weights
+			print 'Loss: ', batchLoss
+			print 'testLoss', testLoss / 2000.0
+			print 'Acc:', counter / 10000.0
+			print 'testAcc: ', testCount / 2000.0	
 		# end #
 	## end ##
 
@@ -127,33 +169,41 @@ class MyOneLayerFullyConnectedNet(Object):
 		nabla_weights = [numpy.zeros(w.shape) for w in self.weights]
 		nabla_biases = [numpy.zeros(b.shape) for b in self.biases]
 		# Calculate error for the output layer
-		self.errors[-1] = [self.lossPrime(o, l) * self.activationPrime[-1](i) for i, o, l, in zip(self.inputs[-1], self.outputs[-1], y)] 
-		nabla_bias[-1] = self.errors[-1]
+		self.errors[-1] = self.outputs[-1] - y #self.lossPrime(self.outputs[-1], y) * self.activationPrime[-1](self.inputs[-1]) 
+		#print 'output: ', self.outputs[-1]
+		#print 'y: ', y 
+		#print 'error[-1]: ', self.errors[-1]
+		#print 'number of layers: ', self.numOfLayers
+		nabla_biases[-1] = self.errors[-1]
 		nabla_weights[-1] = numpy.outer(self.outputs[-2], self.errors[-1])
-		
+		#print 'nabla_weights[-1]: ', nabla_weights[-1]
+		#print 'nabla_weights: ', nabla_weights[1].shape, self.errors[-1].shape
 		# Start backPropagation, calculate from the second-last layers.
-		for layer in xrange(self.numOfLayer - 2, 0, -1):
+		for layer in range(self.numOfLayers - 2, 0, -1):
 			# Note that the w_l equals to the matrix corresponding to the index 'l-1.
-			activPrime = numpy.array([self.activationPrime(i) for i in self.inputs[layer]])
-			self.errors[layer] = numpy.dot(self.weights[layer], self.errors[layer+1]) * activPrime
+			activPrime = self.activationPrime[layer](self.inputs[layer])
+			#print 'prime of activ: ', activPrime.shape	
+			self.errors[layer] = numpy.multiply(numpy.dot(self.weights[layer], self.errors[layer+1]).transpose(), activPrime)
+			#print 'inspect: ', layer, self.errors[layer+1].shape
+			#print 'inspect error: ', self.errors[layer].shape
 			# Calcualte nabla_Loss / nabla_w_l. 
-			nabla_weights[layer] = numpy.outer(self.output[layer-1], self.errors[layer])
+			nabla_weights[layer-1] = numpy.outer(self.outputs[layer-1], self.errors[layer])
 			# Calculate nabla_loss / nabla_b_l.
-			nabla_biases[layer] = self.errors[layer]
-		# end #	
-		
+			nabla_biases[layer-1] = self.errors[layer]
+		# end #
+		#print 'error: ', self.errors[1].shape, self.weights[1].shape
 		return (nabla_weights, nabla_biases)
-
 	## end ##
 	
 	## Data should be predicted one by one.
 	## I do not support batch prediction for now	
 	def prediction(self, data):
 		out = self.feedForward(data)
+		loss = self.lossFunction
 		if out > 1 - out:
-			return 1
-		else
-			return 0
+			return (0, out)
+		else:
+			return (1, out)
 	## end ##
 
 ### end ###
